d843cb4c737b0b92df032f745560701cfc795736
Owaisdfw <Owaisdfw@gmail.com>
2022-01-22 10:36:01 -0600

    Added Feeder Code

--- src/main/java/frc/robot/subsystems/Feeder.java
+++ src/main/java/frc/robot/subsystems/Feeder.java
@@ -1,8 +1,21 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Feeder extends SubsystemBase {
 
+    private WPI_TalonSRX feeder;
+
+    public Feeder(WPI_TalonSRX feeder) {
+        this.feeder = feeder;
+        this.feeder.setInverted(true);
+
+    }
+
+    public void set(double speed) {
+        feeder.set(ControlMode.PercentOutput, speed);
+    }
 }
-    
