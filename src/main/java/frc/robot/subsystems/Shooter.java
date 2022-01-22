188dcaf52dab09f3d62261cecd64483cfb749c84
IsaacBos25 <isaacbosque25@icloud.com>
2022-01-22 10:29:22 -0600

    Added shooter code

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -1,7 +1,21 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Shooter extends SubsystemBase {
 
+    private WPI_TalonSRX shooter;
+
+    public Shooter(WPI_TalonSRX shooter) {
+        this.shooter = shooter;
+        this.shooter.setInverted(true);
+
+    }
+
+    public void set(double speed) {
+        shooter.set(ControlMode.PercentOutput, speed);
+    }
 }
