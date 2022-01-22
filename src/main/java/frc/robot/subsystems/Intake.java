796ad77d9b7163255be085a623c49b0f2a37599c
Addison Pardini <addison.pardini@gmail.com>
2022-01-22 10:20:46 -0600

    Updated Intake

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -1,7 +1,21 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
 public class Intake extends SubsystemBase {
-    
+    private WPI_TalonFX intakeMotor;
+
+    public Intake(WPI_TalonFX motor) {
+        intakeMotor = motor;
+        intakeMotor.setInverted(true);
+        intakeMotor.setNeutralMode(NeutralMode.Brake);
+
+        intakeMotor.configFactoryDefault();
+    }
+
+    public void setSpeed(double speed) {
+        intakeMotor.set(ControlMode.PercentOutput, speed);
+    }
 }
