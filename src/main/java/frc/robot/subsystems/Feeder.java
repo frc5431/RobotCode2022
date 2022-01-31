2ab8d4116f77072f1b75997d1c4ef7b4143d9b13
Owaisdfw <Owaisdfw@gmail.com>
2022-01-31 17:25:26 -0600

    Updated Motors

--- src/main/java/frc/robot/subsystems/Feeder.java
+++ src/main/java/frc/robot/subsystems/Feeder.java
@@ -7,15 +7,18 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Feeder extends SubsystemBase {
 
-    private WPI_TalonSRX feeder;
+    private WPI_TalonSRX feeder1, feeder2; 
 
-    public Feeder(WPI_TalonSRX feeder) {
-        this.feeder = feeder;
-        this.feeder.setInverted(true);
+    public Feeder(WPI_TalonSRX feeder1, WPI_TalonSRX feeder2) {
+        this.feeder1 = feeder1;
+        this.feeder1.setInverted(true);
 
+        this.feeder2 = feeder2;
+        this.feeder2.setInverted(true);
     }
 
     public void set(double speed) {
-        feeder.set(ControlMode.PercentOutput, speed);
+        feeder1.set(ControlMode.PercentOutput, speed);
+        feeder2.set(ControlMode.PercentOutput, speed);
     }
 }
