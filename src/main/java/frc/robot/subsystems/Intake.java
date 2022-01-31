2e43f768d27259750fdeac354c08f0f61c966b0b
Addison Pardini <addison.pardini@gmail.com>
2022-01-31 16:37:44 -0600

    Updated Intake for 2 Motors

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -5,17 +5,21 @@ import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class Intake extends SubsystemBase {
-    private WPI_TalonFX intakeMotor;
+    private WPI_TalonFX intakeMotor1, intakeMotor2;
 
     public Intake(WPI_TalonFX motor) {
-        intakeMotor = motor;
-        intakeMotor.setInverted(true);
-        intakeMotor.setNeutralMode(NeutralMode.Brake);
+        intakeMotor1 = motor;
+        intakeMotor1.setInverted(true);
+        intakeMotor1.setNeutralMode(NeutralMode.Brake);
 
-        intakeMotor.configFactoryDefault();
+        intakeMotor2 = motor;
+        intakeMotor2.setInverted(true);
+        intakeMotor2.setNeutralMode(NeutralMode.Brake);
+
     }
 
     public void setSpeed(double speed) {
-        intakeMotor.set(ControlMode.PercentOutput, speed);
+        intakeMotor1.set(ControlMode.PercentOutput, speed);
+        intakeMotor2.set(ControlMode.PercentOutput, speed);
     }
 }
