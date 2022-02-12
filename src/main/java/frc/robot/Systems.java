eaf9f1a6852f830bd2dabf930d17cf990f9fcb90
Aahana Shrivastava <aahanadoesrobotics@gmail.com>
2022-02-12 10:01:56 -0600

    Updated motor names

--- src/main/java/frc/robot/Systems.java
+++ src/main/java/frc/robot/Systems.java
@@ -10,8 +10,8 @@ import frc.robot.subsystems.Pivot;
 import frc.robot.subsystems.Shooter;
 
 public class Systems {
-    private WPI_TalonSRX feeder1;
-    private WPI_TalonSRX feeder2;
+    private WPI_TalonSRX feederBottom;
+    private WPI_TalonSRX feederTop;
     private WPI_TalonSRX shooter1;
     private WPI_TalonSRX shooter2;
     private WPI_TalonFX intake_motor1;
@@ -27,7 +27,7 @@ public class Systems {
     private Climber climber;
 
     public Systems() {
-        feeder = new Feeder(feeder1,feeder2);
+        feeder = new Feeder(feederBottom,feederTop);
         shooter = new Shooter(shooter1, shooter2);
         intake = new Intake(intake_motor1, intake_motor2);
         pivot = new Pivot(pivot_motor);
--- src/main/java/frc/robot/subsystems/Feeder.java
+++ src/main/java/frc/robot/subsystems/Feeder.java
@@ -7,18 +7,18 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Feeder extends SubsystemBase {
 
-    private WPI_TalonSRX feeder1, feeder2; 
+    private WPI_TalonSRX feederBottom, feederTop; 
 
-    public Feeder(WPI_TalonSRX feeder1, WPI_TalonSRX feeder2) {
-        this.feeder1 = feeder1;
-        this.feeder1.setInverted(true);
+    public Feeder(WPI_TalonSRX feederBottom, WPI_TalonSRX feederTop) {
+        this.feederBottom = feederBottom;
+        this.feederBottom.setInverted(true);
 
-        this.feeder2 = feeder2;
-        this.feeder2.setInverted(true);
+        this.feederTop = feederTop;
+        this.feederTop.setInverted(true);
     }
 
     public void set(double speed) {
-        feeder1.set(ControlMode.PercentOutput, speed);
-        feeder2.set(ControlMode.PercentOutput, speed);
+        feederBottom.set(ControlMode.PercentOutput, speed);
+        feederTop.set(ControlMode.PercentOutput, speed);
     }
 }
