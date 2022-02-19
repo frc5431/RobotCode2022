fbe5086cd182416c15a9686025acb97d9232ef74
test <60162283+5431-programming@users.noreply.github.com>
2022-02-19 13:04:39 -0600

    Refactor, add buttons

--- src/main/java/frc/robot/commands/subsystems/FeederTopCommand.java
+++ src/main/java/frc/robot/commands/subsystems/FeederTopCommand.java
@@ -3,33 +3,30 @@ package frc.robot.commands.subsystems;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Systems;
 import frc.robot.subsystems.Feeder;
-import frc.robot.subsystems.Shooter;
 import frc.team5431.titan.core.misc.Logger;
 
 /**
  * @author Ryan Hirasaki
  */
-public class FeederCommand extends CommandBase {
+public class FeederTopCommand extends CommandBase {
     private final Feeder feeder;
     private final boolean direction;
 	private final double speed;
-	private final Shooter shooter;
-	private boolean rpmWait;  
 
-
-    public FeederCommand(Systems systems, double speed,  boolean rpmWait) {
-        this(systems, speed, false, rpmWait);
+    public FeederTopCommand(Systems systems, boolean reverse) {
+        this(systems, Feeder.DEFAULT_SPEED, reverse);
     }
 
-    public FeederCommand(Systems systems, double speed, boolean reverse, boolean rpmWait) {
+    public FeederTopCommand(Systems systems, double speed) {
+        this(systems, speed, false);
+    }
+
+    public FeederTopCommand(Systems systems, double speed, boolean reverse) {
         this.feeder = systems.getFeeder();
         this.direction = reverse;
 		this.speed = speed;
-		this.shooter = systems.getShooter();
-		this.rpmWait = rpmWait; 
-		 
 
-        addRequirements(feeder);
+        addRequirements(feeder.getTop());
     }
 
     @Override
@@ -39,25 +36,13 @@ public class FeederCommand extends CommandBase {
 	
 	@Override
 	public void execute() {
-
-		if(direction) {
-
-			if (shooter.atVelocity() || !rpmWait) {
-				feeder.set(speed);
-			} else {
-				Logger.l("Shooter not at speed, not pushing up!");
-				feeder.set(0);
-			}
-		}
-		else {
-			feeder.set(-speed);
-		}
+        feeder.setTop(direction ? speed : -speed);
 	}
 
     @Override
     public void end(boolean interrupted) {
         Logger.l("Feeder Command Done");
-        feeder.set(0);
+        feeder.setTop(0);
     }
     
     @Override
