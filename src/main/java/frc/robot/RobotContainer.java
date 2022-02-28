73729da96332156622333dfb248b0b149aa59c78
test <60162283+5431-programming@users.noreply.github.com>
2022-02-28 13:56:17 -0600

    Attempt to bind buttons

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -97,12 +97,12 @@ public class RobotContainer {
                 .whileHeld(new IntakeCommand(systems, true));
         
         // Pivot Up
-        new JoystickButton(buttonBoard, 5)
+        new JoystickButton(buttonBoard, 16)
                 .whileHeld(() -> systems.getPivot().set(0.1), systems.getPivot())
                 .whenReleased(() -> systems.getPivot().set(0), systems.getPivot());
         
         // Pivot Down
-        new JoystickButton(buttonBoard, 2)
+        new JoystickButton(buttonBoard, 13)
                 .whileHeld(() -> systems.getPivot().set(-0.1), systems.getPivot())
                 .whenReleased(() -> systems.getPivot().set(0), systems.getPivot());
 
