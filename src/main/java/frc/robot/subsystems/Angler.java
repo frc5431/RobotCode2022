346c29232a88717978cefda82a338d36a8acc89b
test <60162283+5431-programming@users.noreply.github.com>
2022-02-28 16:28:05 -0600

    Speed up feeder

--- src/main/java/frc/robot/subsystems/Angler.java
+++ src/main/java/frc/robot/subsystems/Angler.java
@@ -4,7 +4,11 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj.Servo;
 
 public class Angler extends SubsystemBase {
-
+/**
+     * @author Colin Wong
+     * @author Aahana Shrivastava
+     * @author Addison Pardini
+     */
     private Servo anglerServo;
     
     public Angler(Servo servo) {
