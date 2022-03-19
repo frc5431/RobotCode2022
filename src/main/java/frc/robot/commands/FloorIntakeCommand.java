11e1b68ba1c09ae586fc94c18b76fd2b495304f7
Colin Wong <cwongmath@gmail.com>
2022-03-18 20:18:17 -0500

    Separate intake mostly from floor intake
    basically if something else runs feeder, like shoot, the floorintakecommand wont get interrupted - only the feeder command of the floor intake will. this allows for running floorintake and shoot simultaneously.

--- src/main/java/frc/robot/commands/FloorIntakeCommand.java
+++ src/main/java/frc/robot/commands/FloorIntakeCommand.java
@@ -1,6 +1,7 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.ProxyScheduleCommand;
 import frc.robot.Systems;
 import frc.robot.commands.subsystems.FeederBottomCommand;
 import frc.robot.commands.subsystems.IntakeCommand;
@@ -8,7 +9,7 @@ import frc.robot.commands.subsystems.IntakeCommand;
 public class FloorIntakeCommand extends ParallelCommandGroup {
     public FloorIntakeCommand(Systems systems) {
         addCommands(
-            new IntakeCommand(systems, false),
+            new ProxyScheduleCommand(new IntakeCommand(systems, false)),
             new FeederBottomCommand(systems, false)
         );
     }
