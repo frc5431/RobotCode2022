8b0e8d5eb2d90ad52d68e405a0041608759e326a
test <60162283+5431-programming@users.noreply.github.com>
2022-03-29 19:49:38 -0500

    angler values

--- src/main/java/frc/robot/subsystems/Angler.java
+++ src/main/java/frc/robot/subsystems/Angler.java
@@ -13,8 +13,8 @@ import edu.wpi.first.wpilibj.Servo;
 public class Angler extends SubsystemBase {
 
     public static final double DEFAULT_SPEED = 0.05;
-    public static final double DOWN_LIMIT = 0.3;
-    public static final double UP_LIMIT = 0.8;
+    public static final double DOWN_LIMIT = 0.2;
+    public static final double UP_LIMIT = 0.7;
 
     private Servo anglerServo;
     
