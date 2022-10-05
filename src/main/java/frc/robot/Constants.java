d29bdd047017653062f7e203521793e8ca6a797c
test <60162283+5431-programming@users.noreply.github.com>
2022-10-05 18:37:46 -0500

    Alignment and tuning

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -59,22 +59,22 @@ public final class Constants {
     public static final int FRONT_LEFT_MODULE_DRIVE_MOTOR = 2;
     public static final int FRONT_LEFT_MODULE_STEER_MOTOR = 1;
     public static final int FRONT_LEFT_MODULE_STEER_ENCODER = 9;
-    public static final double FRONT_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(85.430);
+    public static final double FRONT_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(81.562); // 85.430
 
     public static final int FRONT_RIGHT_MODULE_DRIVE_MOTOR = 8;
     public static final int FRONT_RIGHT_MODULE_STEER_MOTOR = 7;
     public static final int FRONT_RIGHT_MODULE_STEER_ENCODER = 12;
-    public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(140.449);
+    public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(139.482); // 140.449
 
     public static final int BACK_LEFT_MODULE_DRIVE_MOTOR = 4;
     public static final int BACK_LEFT_MODULE_STEER_MOTOR = 3;
     public static final int BACK_LEFT_MODULE_STEER_ENCODER = 10;
-    public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(49.307); // 49.482 + 49.307
+    public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(50.098); // 49.307
 
     public static final int BACK_RIGHT_MODULE_DRIVE_MOTOR = 6;
     public static final int BACK_RIGHT_MODULE_STEER_MOTOR = 5;
     public static final int BACK_RIGHT_MODULE_STEER_ENCODER = 11;
-    public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(30.234);
+    public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(29.443); // 30.234
 
     public static final double CAMERA_HEIGHT_METERS = Units.inchesToMeters(38.9);
     public static final double TARGET_HEIGHT_METERS = Units.feetToMeters(8) + Units.inchesToMeters(8);
