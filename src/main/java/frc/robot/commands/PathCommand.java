13b76d7860031d8ad2f1cf33934e768a89854a62
test <60162283+5431-programming@users.noreply.github.com>
2022-11-14 19:30:44 -0600

    Fix PIDController

--- src/main/java/frc/robot/commands/PathCommand.java
+++ src/main/java/frc/robot/commands/PathCommand.java
@@ -5,9 +5,7 @@ import com.pathplanner.lib.PathPlannerTrajectory;
 import com.pathplanner.lib.commands.PPSwerveControllerCommand;
 
 import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
-import edu.wpi.first.math.trajectory.TrapezoidProfile.Constraints;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Systems;
@@ -30,7 +28,7 @@ public class PathCommand extends SequentialCommandGroup {
                     drivebase.m_kinematics, 
                     new PIDController(0.7, 0, 0),  // TODO: Characterize swerve to fix PID constants
                     new PIDController(0.7, 0, 0), 
-                    new ProfiledPIDController(1.5, 0, 0, new Constraints(Drivebase.MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND, 3.14)),
+                    new PIDController(1.5, 0, 0),
                     (states) -> drivebase.driveRaw(multiply(drivebase.m_kinematics.toChassisSpeeds(states), DRIVEBASE_SPEED_MULT)), 
                     drivebase),
             new InstantCommand(
