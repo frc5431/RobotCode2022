f608fe7ec5d7a62fb1ed1692dcfe4eb28353ce4a
Colin Wong <cwongmath@gmail.com>
2022-11-17 16:45:44 -0600

    Use static Commands factories

--- src/main/java/frc/robot/subsystems/Feeder.java
+++ src/main/java/frc/robot/subsystems/Feeder.java
@@ -8,6 +8,8 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.StartEndCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+import static edu.wpi.first.wpilibj2.command.Commands.*;
+
 public class Feeder extends SubsystemBase {
 
     public static final double DEFAULT_SPEED_BOTTOM = 1.0;  // 0.9 // 0.7 // 0.6
@@ -46,8 +48,10 @@ public class Feeder extends SubsystemBase {
     }
 
     public Command feedEverythingCommand(boolean reverse) {
-        return getBottom().runFeederCommand(reverse).alongWith(
-               getTop().runFeederCommand(reverse));
+        return parallel(
+            getBottom().runFeederCommand(reverse),
+            getTop().runFeederCommand(reverse)
+        );
     }
 
     public class FeederBase extends SubsystemBase {
